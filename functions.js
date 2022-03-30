5049e9ee512ad6170f991940297c3464fdf0114a
chanlee20 <70008295+chanlee20@users.noreply.github.com>
2022-03-30 13:37:16 -0500

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,62 @@
+function open(event) {
+    id = event.target.id;
+    console.log(id);
+    document.getElementById('right').style.display = 'inline';
+    document.getElementById('left').style.display = 'inline';
+    let img = document.getElementsByClassName("img");
+    if(id == 1){
+        document.getElementById('left').style.display = 'none';
+    }
+    if(id == 9){
+        document.getElementById('right').style.display = 'none';
+    }
+    document.getElementById("largeimg").src = img[id-1].getAttribute("src");
+    document.getElementById('modal-container').style.display = 'inline';
+    document.getElementById('right').addEventListener("click", nextImg);
+    document.getElementById('left').addEventListener("click", prevImg);
+}
+
+function nextImg() {
+        id++;
+        console.log("next img: " + id);
+        let img = document.getElementsByClassName("img");
+        document.getElementById("largeimg").src = img[id-1].getAttribute("src");
+        document.getElementById('right').style.display = 'inline';
+
+        if(id == 9) {
+            document.getElementById('right').style.display = 'none';
+        }
+        if(id != 9){
+            document.getElementById('left').style.display = 'inline';
+        }
+        
+}
+
+function prevImg() {
+        id--;
+        console.log("prev img: " + id);
+        let img = document.getElementsByClassName("img");
+        document.getElementById("largeimg").src = img[id-1].getAttribute("src");
+        if(id == 1) {
+            document.getElementById('left').style.display = 'none';
+        }
+        if(id != 1){
+            document.getElementById('right').style.display = 'inline';
+        }
+}
+
+function close() {
+    document.getElementById('modal-container').style.display = 'none';
+}
+
+function addId(){
+    for(let i = 1; i <= 9; i++){
+        document.getElementById(i.toString()).addEventListener("click", open);
+    }
+    document.getElementById('modal-container').style.display = 'none';
+
+}
+
+document.getElementById("close").addEventListener("click", close);
+
+addId();
